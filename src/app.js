9d88276f3ffd61860b26d8d8aa92fcfcb0c368d5
Melanie Dana Medigovich <melacem@gmail.com>
2025-04-03 14:43:44 +0000

    generador de excusas final

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
+window.onload = function () {
   generateExcuse();
   document.getElementById("generateButton").addEventListener("click", generateExcuse);
 };
@@ -11,12 +11,12 @@ function generateExcuse() {
   let accion = ["se accidentó", "se enfermó", "se averió", "se perdió"];
   let cuando = ["ayer", "anoche", "esta mañana", "hace una hora"];
   let que = ["y debo llevarla al hospital", "y debo cuidarla", "y debo llevarlo al taller", "y debo encontrarlo"];
-  
+
   let excuse =
     `${quien[Math.floor(Math.random() * quien.length)]} ` +
     `${accion[Math.floor(Math.random() * accion.length)]} ` +
     `${cuando[Math.floor(Math.random() * cuando.length)]} ` +
     `${que[Math.floor(Math.random() * que.length)]}.`;
-  
+
   document.getElementById("excuse").innerText = excuse;
 }
